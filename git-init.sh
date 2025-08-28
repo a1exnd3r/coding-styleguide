3d74c32488b31df07957f09061c7de9d0b0ae30b
Alex Olsson <alex.olsson@arratech.com>
2025-08-28 15:37:19 +0200

    wip: updates and improvements

--- git-init.sh
+++ git-init.sh
@@ -6,4 +6,4 @@ git init
 git add README.md
 git commit -m "Initial commit: My Coding Styleguide"
 git remote set-url origin https://github.com/a1exnd3r/coding-styleguide.git
-git push -u origin mster
+git push -u origin master
